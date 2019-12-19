91a8a801d6969412798d39944c5bc839f36feac9
brettmadrid <brettmadrid9@gmail.com>
2019-12-18 18:26:18 -0800

    commented dll_stack.py

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -7,16 +7,17 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        # faster than an array at insertion and deletions
         self.storage = DoublyLinkedList()
 
     def push(self, value):
-        self.storage.add_to_head(value)
+        self.storage.add_to_head(value)  # call dll method and pass in value
         self.size += 1
 
     def pop(self):
         if self.size > 0:
             self.size -= 1
-            return self.storage.remove_from_head()
+            return self.storage.remove_from_head()  # call dll method
 
     def len(self):
         return self.size
