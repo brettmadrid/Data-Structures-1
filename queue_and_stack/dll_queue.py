45ba385de7ddea043d2481854a1df1d7136c8c6e
brettmadrid <brettmadrid9@gmail.com>
2019-12-18 18:25:14 -0800

    commented dll_queue.py

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -9,13 +9,14 @@ class Queue:
         self.storage = DoublyLinkedList()  # instantiate an empty dll
 
     def enqueue(self, value):
+        # call method from dll and pass in new value
         self.storage.add_to_tail(value)
         self.size += 1
 
     def dequeue(self):
-        if self.size > 0:
+        if self.size > 0:  # if the list is not empty
             self.size -= 1
-            return self.storage.remove_from_head()
+            return self.storage.remove_from_head()  # call method from dll
         else:
             return None
 
