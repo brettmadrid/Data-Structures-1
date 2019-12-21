c5aa206cc72a4b4c7f2d7485bf468c43bc175f24
brettmadrid <brettmadrid9@gmail.com>
2019-12-21 14:33:56 -0800

    added move_to_front and move_to_end methods

--- queue_and_stack/doubly_linked_list.py
+++ queue_and_stack/doubly_linked_list.py
@@ -120,13 +120,35 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        if node == self.head:
+            return
+        if node == self.tail:
+            self.remove_from_tail()
+            self.add_to_head(node.value)
+        else:
+            val = node.value
+            prevNode = node.prev
+            nextNode = node.next
+            prevNode.next = nextNode
+            nextNode.prev = prevNode
+            self.add_to_head(val)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        if node = self.tail:
+            return
+        if node = self.head:
+            self.remove_from_head()
+            self.add_to_tail(node.value)
+        else:
+            val = node.value
+            prevNode = node.prev
+            nextNode = node.next
+            prevNode.next = nextNode
+            nextNode.prev = prevNode
+            self.add_to_tail(val)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
